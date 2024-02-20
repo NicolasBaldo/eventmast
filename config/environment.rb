16ccacc56d3292990d28fab0572ffc935308f786
NicolasBaldo <septfoisept@outlook.fr>
2024-02-20 14:23:34 +0100

    oaaaaa

--- config/environment.rb
+++ config/environment.rb
@@ -11,7 +11,7 @@ require 'dotenv/load'
 ActionMailer::Base.smtp_settings = {
     :user_name => ENV['MAILJET_LOGIN'],
     :password => ENV['MAILJET_PWD'],
-    :domain => 'www.eventmast.com',
+    :domain => 'google.com',
     :address => 'in-v3.mailjet.com',
     :port => 587,
     :authentication => :plain,
