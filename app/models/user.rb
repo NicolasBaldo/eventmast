a637e00f987ad9077b1ff0f7b54ac0195b017ba0
NicolasBaldo <septfoisept@outlook.fr>
2024-02-20 10:11:44 +0100

    procfilee

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
     has_many :participations
     has_many :events, through: :participations
   
-    after_action :welcome_send, only: [:create]
+
 
   
     def welcome_send
